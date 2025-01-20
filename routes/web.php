02a29e097de9d14d068bf091442e4fe3dfbd26df
Reshzy <reshzy581@gmail.com>
2025-01-20 15:34:46 +0800

    Login Register successfully added

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
-// returns the home page with all posts
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// returns the form for adding a post
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// adds a post to the database
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// returns a page that shows a full post
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-
-// returns the form for editing a post
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-
-// updates a post
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-
-// deletes a post
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+require __DIR__.'/auth.php';
